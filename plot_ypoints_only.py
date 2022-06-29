712e6416b8da9d98328abf780a9227290d0dbc5a
AMO15310 <wachiraamos402@gmail.com>
2022-06-30 00:57:57 +0300

    plot a graph only using the y axis values

---
+++ plot_ypoints_only.py
@@ -0,0 +1,9 @@
+#import the matplotlib module and the pyplot submodule
+import matplotlib.pyplot as plt
+#import numpy module
+import numpy as np
+#define the y axis values as an array
+ypoints = np.array([45,58,87,98,101])
+#plot the graph and show to view
+plt.plot(ypoints)
+plt.show()
